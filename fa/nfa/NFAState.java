d2b98f8a514c3e3fcd5adc548c9c02275d4643a6
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-04-11 12:56:29 -0600

    Redesigned NFAState

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,8 +1,11 @@
 package fa.nfa;
 
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  * Class that represents a single state in a non-deterministic finite automata.
- * Extends the functionality of the abstract State class. Used by NFA instances 
+ * Extends the functionality of the abstract State class. Used by NFA instances
  * to represent the IO nature of states in finite state machines.
  * 
  * @author Juan Becerra
@@ -10,43 +13,59 @@ package fa.nfa;
  */
 public class NFAState extends fa.State {
 
-    // Hash code used in 
-    private final int hashCode;
+    private HashMap<Character, Set<NFAState>> delta;
+    private boolean isFinal;
 
     /**
-     * Constructs a DFAState object given a unique name.
-     * initializes a hash code based on the name.
+     * Constructs an NFAState.
+     * 
+     * Sets a name, a delta transition function that returns a set of NFA States
+     * given a character, and sets final-status to false.
      * 
      * @param name
      */
     public NFAState(String name) {
         this.name = name;
-        this.hashCode = name.hashCode();
+        delta = new HashMap<Character, Set<NFAState>>();
+        isFinal = false;
     }
 
     /**
-     * Returns a hashcode based on the DFAState's name
+     * Returns whether or not state is final
      * 
-     * This is used to ensure that there are never duplicate states in DFA's sets
-     * 
-     * @return hashCode
+     * @return true if final, otherwise false
      */
-    @Override
-    public int hashCode() {
-        return hashCode;
+    public boolean isFinal() {
+        return isFinal;
     }
 
     /**
-     * Returns whether or not two DFAState objects are the same, based on their names.
+     * Adds a transition 
      * 
-     * This is used to ensure that there are never duplicate states in DFA's sets
-     * 
-     * @param DFAState object
+     * @param onSymb
+     * @param toState
      */
-    @Override
-    public boolean equals(Object object) {
-        NFAState otherState = (NFAState) object;
-        return this.hashCode == otherState.hashCode();
+    public void addTransition(char onSymb, NFAState toState) {
+        // TODO, we'll need to query the hashset to see if it exists.
+        // If so, add toState state to list of transition states
+        // If not, create set and add the toState
+
+        //delta.put(onSymb, toState);
+    }
+
+    /**
+     * Returns set of states of where this state can go given a symbol
+     * 
+     * @param symb
+     * @return set of states if valid, error if cannot make transition
+     */
+    public Set<NFAState> getTo(char symb) {
+        Set<NFAState> ret = delta.get(symb);
+        if(ret == null){
+            System.err.println("ERROR: DFAState.getTo(char symb) returns null on " + symb + " from " + name);
+            System.exit(2);
+        }
+        return ret;
     }
 
 }
